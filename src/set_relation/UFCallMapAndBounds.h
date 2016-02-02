0e566b6afd8a485b9602d38cdbc0b71ee57162c1
Mahdi Soltan Mohammadi <kingmahdi@email.arizona.edu>
2016-02-02 09:12:58 -0700

    UFCallMap in separate files

--- src/set_relation/UFCallMapAndBounds.h
+++ src/set_relation/UFCallMapAndBounds.h
@@ -96,47 +96,6 @@ private:
     int                                 mInitialArity;    
 };
 
-/*!
- * \class UFCmap
- * This class can store UFcalls and their equ. symbolic constants. 
- * We need this in functionality of creating affine sets out of
- * non-affine sets, where UFcalls get replaced with symbolic constants.
- */
-class UFCmap {
-public:
-    UFCmap(){}
-    ~UFCmap(){
-               mUFC2Str.clear();
-               mStr2UFC.clear();
-    }
-
-    //! Copy constructor.
-    UFCmap( const UFCmap& other ){ *this = other; }
-
-    //! returns a string representing ufcterm as a symbolic constant
-    string symUFC( std::string &ufcName );
-
-    //! Use this to insert a UFC term to maps.
-    //  The function creats an string representing the UFC then,
-    //  adds (ufc,str) to mUFC2Str & adds (ufc,str) to mStr2UFC
-    void insert( UFCallTerm &ufcterm );
-
-    //! Searches for ufcterm in mUFC2Str. If it exists in the map, returns
-    // the equ. symbol, otherwise returns an empty string.
-    string find( UFCallTerm &ufcterm );
-
-    //! Searches for a symbol in mStr2UFC. If it exists in the map, returns the
-    //  equ. UFC, otherwise returns foo() (representing empty UFC)
-    UFCallTerm find( string &symbol );
-
-    // prints the content of the map into a string, and returns it
-    std::string toString();
-
-private:
-    std::map<UFCallTerm,string> mUFC2Str;
-    std::map<string,UFCallTerm> mStr2UFC;
-};
-
 }
 
 #endif
