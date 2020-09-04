fc27c176751828d2e6f60271bc3a856f06db1b24
Anna Rift <ultimateartificer@gmail.com>
2020-09-03 20:25:11 -0600

    Add some of the sparse format examples

--- tutorial/sparse_format_example.cc
+++ tutorial/sparse_format_example.cc
@@ -17,13 +17,50 @@
 #include <iegenlib.h>
 
 using iegenlib::Set;
+using iegenlib::Relation;
 
 int main(int ac, char **av)
 {
-//  iegenlib::Set* set = new iegenlib::Set("{[a]: a=10}");
-  Set* set = new Set("{[a]:0<a}");
 
-  std::cout << set->prettyPrintString() << std::endl;
+  std::cout << "Starting example COO_CSR_1" << std::endl;
+  Relation *r_coo = new Relation("{[n] -> [i,j] : row(n) = i && col(n) = j "
+                                 "&& 0 <= n && n < NNZ }");
+  std::cout << "COO relation: " << r_coo->prettyPrintString() << std::endl;
+  Relation *r_csr = new Relation("{[n] -> [i,j] :n >= rptr(i) "
+                                 "&& n < rptr(i+1) && rowcol_inv(i,j) = n "
+                                 "&& 0 <= n && n < NNZ }");
+  std::cout << "CSR relation: " << r_csr->prettyPrintString() << std::endl;
+  Relation *r_csr_inv = r_csr->Inverse();
+  std::cout << "CSR relation inverse: " << r_csr_inv->prettyPrintString() << std::endl;
+  Relation *r_coo_csr_comp = r_csr_inv->Compose(r_coo);
+  std::cout << "Composition of CSR inverse and COO relations: " << r_coo_csr_comp->prettyPrintString() << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "Starting example COO_DENSE_COMPOSE" << std::endl;
+  Relation *dense = new Relation("{ [n1] -> [i,j] :"
+                                 " n1 = i * 9 + j }");
+  std::cout << "Dense relation: " << dense->prettyPrintString() << std::endl;
+  Relation *coo_inv = new Relation("{[i,j] -> [n2] : rowcol_inv(n2,0) = i && rowcol_inv(n2,1) = j "
+                                   "&& 0 <= n2 && n2 < NNZ }");
+  std::cout << "COO inverse relation: " << coo_inv->prettyPrintString() << std::endl;
+  Relation *result = coo_inv->Compose(dense);
+  std::cout << "Composition of COO inverse and dense relation: " << result->prettyPrintString() << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "Starting example COO_BCSR" << std::endl;
+  Relation *Flatten = new Relation("{ [n] -> [ti,tj,iz,jz,in,zn,riz,rjz,rin,rjn] :"
+                                   " n = iz * 9 + jz }");
+  std::cout << "Flatten relation: " << Flatten->prettyPrintString() << std::endl;
+  Relation *Zeros = new Relation(
+		  " { [i,j] -> [ti, tj, iz, jz, in, zn, riz, rjz, rin, rjn]: "
+		  " Dense (i,j) = 0 && Dense(in,jn) != 0  && iz = i && jz = j"
+		  " && i = ti * 9 + riz && j = ti * 7 + rjz && in = ti * 9 + rin"
+		  " && in = ti * 7 + rin && 0 <= riz && riz <= 9 && 0 <= rjz "
+		  " && rjz <= 7 && 0 <= rin && rin <= 9 && 0 <= rjz && rjz<=7"
+		  "}"
+		  );
+  std::cout << "Zeroes relation: " << Zeros->prettyPrintString() << std::endl;
+
 
   return 0;
 }
