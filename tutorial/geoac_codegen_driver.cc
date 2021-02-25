58e7d074fa74dfa36f0296f4ddccca44b85a9736
Ravi <ravishankar@u.boisestate.edu>
2021-02-25 10:05:05 -0700

    Testing codegen with append

--- tutorial/geoac_codegen_driver.cc
+++ tutorial/geoac_codegen_driver.cc
@@ -6,8 +6,8 @@
 using iegenlib::Computation;
 using namespace std;
 
-Computation Find_Segment_Computation();
-Computation Eval_Spline_f_Computation();
+Computation* Find_Segment_Computation();
+Computation* Eval_Spline_f_Computation();
 
 
 /**
@@ -16,8 +16,7 @@ Computation Eval_Spline_f_Computation();
 int main(int argc, char **argv){
 
 
-  Computation FindSegment = Find_Segment_Computation();
-  Computation EvalSplineF = Find_Segment_Computation();
+  Computation* EvalSplineF = Eval_Spline_f_Computation();
   
   // vector < pair<string, string> > dataReads;
   // vector < pair<string, string> > dataWrites;
@@ -31,68 +30,16 @@ int main(int argc, char **argv){
   //Calling toDot() on the Computation structure
   fstream dotFileStream;
   cout << "Entering toDot()" << "\n";
-  FindSegment.toDot(dotFileStream,"find_segment_dot.txt");
+  EvalSplineF->toDot(dotFileStream,"eval_spline_f_dot.txt");
 
   //Write codegen to a file
   ofstream outStream;
-  outStream.open("find_segment_codegen.cpp");
-  outStream << FindSegment.codeGen();
+  outStream.open("eval_spline_f_codegen.cpp");
+  outStream << EvalSplineF->codeGen();
   outStream.close();
 
   return 0;
 }
-
-
-
-/*
-  int Find_Segment(double x, double* x_vals, int length, int & prev){
-
-      for(int i = 0; i < length; i++){
-          if(x >= x_vals[i] && x <= x_vals[i+1]){
-              prev = i;
-          }
-      }
-      return prev;        
-  }
-*/
-Computation Find_Segment_Computation(){
-
-  Computation FindSegmentComputation;
-
-  //Creating statement1
-  //if(x >= x_vals[i] && x <= x_vals[i+1]) prev = i;
-  Stmt statement1("if(x >= x_vals[i] && x <= x_vals[i+1]) prev = i",
-      "{[i]: i>=0 && i<length}",  //Iteration schedule - Only happening one time (not iterating)
-      "{[i]->[0, i, 0]}", //Execution schedule - scheduling statement to be first (scheduling function)
-      {},
-      {}
-      );
-  cout << "Source statement : " << statement1.getStmtSourceCode() << "\n\t"
-    <<"- Iteration Space : "<< statement1.getIterationSpace()->prettyPrintString() << "\n\t"
-    << "- Execution Schedule : "<< statement1.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
-
-  //Adding statement2
-  FindSegmentComputation.addStmt(statement1);
-
-  //Creating statement2
-  //return prev; 
-  Stmt statement2("return prev",
-      "{[]}",  //Iteration schedule - Only happening one time (not iterating)
-      "{[]->[1, 0, 0]}", //Execution schedule - scheduling statement to be first (scheduling function)
-      {},
-      {}
-      );
-  cout << "Source statement : " << statement2.getStmtSourceCode() << "\n\t"
-    <<"- Iteration Space : "<< statement2.getIterationSpace()->prettyPrintString() << "\n\t"
-    << "- Execution Schedule : "<< statement2.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
-
-  //Adding statement2
-  FindSegmentComputation.addStmt(statement2);
-
-
-  return FindSegmentComputation;
-}
-
 
 
 /*
@@ -120,115 +67,189 @@ Computation Find_Segment_Computation(){
     return result;
   }
 */
-Computation Eval_Spline_f_Computation(){
+Computation* Eval_Spline_f_Computation(){
 
-  Computation EvalSplineFComputation;
+  Computation* EvalSplineFComputation = new Computation();
 
-  //Creating statement1
+  //Creating s0
   //int k = Find_Segment(x, Spline.x_vals, Spline.length, Spline.accel);
-  Stmt statement1("int k = Find_Segment(x, Spline.x_vals, Spline.length, Spline.accel)",
+  Stmt s0("int k",
       "{[]}",  //Iteration schedule - Only happening one time (not iterating)
-      "{[]->[0]}", //Execution schedule - scheduling statement to be first (scheduling function)
+      "{[0]->[0, 0, 0]}", //Execution schedule - scheduling statement to be first (scheduling function)
       {},
       {}
       );
-  cout << "Source statement : " << statement1.getStmtSourceCode() << "\n\t"
-    <<"- Iteration Space : "<< statement1.getIterationSpace()->prettyPrintString() << "\n\t"
-    << "- Execution Schedule : "<< statement1.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
+  cout << "Source statement : " << s0.getStmtSourceCode() << "\n\t"
+    <<"- Iteration Space : "<< s0.getIterationSpace()->prettyPrintString() << "\n\t"
+    << "- Execution Schedule : "<< s0.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
 
-  //Adding statement2
-  EvalSplineFComputation.addStmt(statement1);
+  //Adding s0
+  EvalSplineFComputation->addStmt(s0);
 
-  //Creating statement2
+  // Creating s1
+  // int k = Find_Segment(x, Spline.x_vals, Spline.length, Spline.accel);
+  // Find_Segment(double x, double* x_vals, int length, int & prev){
+  Stmt s1("double* x_vals = Spline.x_vals; int length = Spline.length; int& prev = Spline.accel",
+    "{[0]}",
+    "{[0]->[1, 0, 0]}",
+    {},
+    {}
+    );
+
+  cout << "Source statement : " << s1.getStmtSourceCode() << "\n\t"
+    <<"- Iteration Space : "<< s1.getIterationSpace()->prettyPrintString() << "\n\t"
+    << "- Execution Schedule : "<< s1.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
+
+  //Adding s1
+  EvalSplineFComputation->addStmt(s1);
+
+  //Call Find_Segment_Computation() 
+  Computation* FindSegmentComputation = Find_Segment_Computation();
+
+  //Append FindSegmentComputation to EvalSplineFComputation
+  int retVal = EvalSplineFComputation->appendComputation(FindSegmentComputation);
+
+/*
+  //Creating s2
   //if (k >= Spline.length) return 0.0; 
-  Stmt statement2("if (k >= Spline.length) return 0.0",
+  Stmt s2("if (k >= Spline.length) return 0.0",
       "{[]}",  //Iteration schedule - Only happening one time (not iterating)
-      "{[]->[1]}", //Execution schedule - scheduling statement to be first (scheduling function)
+      "{[0]->[1, 0, 0]}", //Execution schedule - scheduling statement to be first (scheduling function)
       {},
       {}
       );
-  cout << "Source statement : " << statement2.getStmtSourceCode() << "\n\t"
-    <<"- Iteration Space : "<< statement2.getIterationSpace()->prettyPrintString() << "\n\t"
-    << "- Execution Schedule : "<< statement2.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
+  cout << "Source statement : " << s2.getStmtSourceCode() << "\n\t"
+    <<"- Iteration Space : "<< s2.getIterationSpace()->prettyPrintString() << "\n\t"
+    << "- Execution Schedule : "<< s2.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
 
-  //Adding statement2
-  EvalSplineFComputation.addStmt(statement2);
+  //Adding s2
+  EvalSplineFComputation->addStmt(s2);
 
-  //Creating statement3
+  //Creating s3
   //double X = (x - Spline.x_vals[k])/(Spline.x_vals[k+1] - Spline.x_vals[k]);
-  Stmt statement3("double X = (x - Spline.x_vals[k])/(Spline.x_vals[k+1] - Spline.x_vals[k])",
+  Stmt s3("double X = (x - Spline.x_vals[k])/(Spline.x_vals[k+1] - Spline.x_vals[k])",
       "{[]}",  //Iteration schedule - Only happening one time (not iterating)
-      "{[]->[2]}", //Execution schedule - scheduling statement to be first (scheduling function)
+      "{[0]->[2, 0, 0]}", //Execution schedule - scheduling statement to be first (scheduling function)
       {{"Spline.x_vals","{[k]->[k]}"}, {"Spline.x_vals","{[k+1]->[k+1]}"}}, //Data reads
       {}  //Data writes
       );
-  cout << "Source statement : " << statement3.getStmtSourceCode() << "\n\t"
-    <<"- Iteration Space : "<< statement3.getIterationSpace()->prettyPrintString() << "\n\t"
-    << "- Execution Schedule : "<< statement3.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
+  cout << "Source statement : " << s3.getStmtSourceCode() << "\n\t"
+    <<"- Iteration Space : "<< s3.getIterationSpace()->prettyPrintString() << "\n\t"
+    << "- Execution Schedule : "<< s3.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
 
-  //Adding statement3
-  EvalSplineFComputation.addStmt(statement3);
+  //Adding s3
+  EvalSplineFComputation->addStmt(s3);
 
-  //Creating statement4
+  //Creating s4
   //double A = Spline.slopes[k] * (Spline.x_vals[k+1] - Spline.x_vals[k]) - (Spline.f_vals[k+1] - Spline.f_vals[k]);
-  Stmt statement4("double A = Spline.slopes[k] * (Spline.x_vals[k+1] - Spline.x_vals[k]) - (Spline.f_vals[k+1] - Spline.f_vals[k])",
+  Stmt s4("double A = Spline.slopes[k] * (Spline.x_vals[k+1] - Spline.x_vals[k]) - (Spline.f_vals[k+1] - Spline.f_vals[k])",
       "{[]}",  //Iteration schedule - Only happening one time (not iterating)
-      "{[]->[3]}", //Execution schedule - scheduling statement to be first (scheduling function)
+      "{[0]->[3, 0, 0]}", //Execution schedule - scheduling statement to be first (scheduling function)
       {{"Spline.slopes","{[k]->[k]}"}, {"Spline.x_vals","{[k]->[k]}"}, {"Spline.x_vals","{[k+1]->[k+1]}"}, {"Spline.f_vals","{[k]->[k]}"}, {"Spline.f_vals","{[k+1]->[k+1]}"}}, //Data reads
       {}  //Data writes
       );
-  cout << "Source statement : " << statement4.getStmtSourceCode() << "\n\t"
-    <<"- Iteration Space : "<< statement4.getIterationSpace()->prettyPrintString() << "\n\t"
-    << "- Execution Schedule : "<< statement4.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
+  cout << "Source statement : " << s4.getStmtSourceCode() << "\n\t"
+    <<"- Iteration Space : "<< s4.getIterationSpace()->prettyPrintString() << "\n\t"
+    << "- Execution Schedule : "<< s4.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
 
-  //Adding statement4
-  EvalSplineFComputation.addStmt(statement4);
+  //Adding s4
+  EvalSplineFComputation.addStmt(s4);
 
-  //Creating statement5
+  //Creating s5
   //double B = -Spline.slopes[k+1] * (Spline.x_vals[k+1] - Spline.x_vals[k]) + (Spline.f_vals[k+1] - Spline.f_vals[k]);
-  Stmt statement5("double B = -Spline.slopes[k+1] * (Spline.x_vals[k+1] - Spline.x_vals[k]) + (Spline.f_vals[k+1] - Spline.f_vals[k])",
+  Stmt s5("double B = -Spline.slopes[k+1] * (Spline.x_vals[k+1] - Spline.x_vals[k]) + (Spline.f_vals[k+1] - Spline.f_vals[k])",
       "{[]}",  //Iteration schedule - Only happening one time (not iterating)
-      "{[]->[4]}", //Execution schedule - scheduling statement to be first (scheduling function)
+      "{[0]->[4, 0, 0]}", //Execution schedule - scheduling statement to be first (scheduling function)
       {{"Spline.slopes","{[k+1]->[k+1]}"}, {"Spline.x_vals","{[k]->[k]}"}, {"Spline.x_vals","{[k+1]->[k+1]}"}, {"Spline.f_vals","{[k]->[k]}"}, {"Spline.f_vals","{[k+1]->[k+1]}"}}, //Data reads
       {}  //Data writes
       );
-  cout << "Source statement : " << statement5.getStmtSourceCode() << "\n\t"
-    <<"- Iteration Space : "<< statement5.getIterationSpace()->prettyPrintString() << "\n\t"
-    << "- Execution Schedule : "<< statement5.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
+  cout << "Source statement : " << s5.getStmtSourceCode() << "\n\t"
+    <<"- Iteration Space : "<< s5.getIterationSpace()->prettyPrintString() << "\n\t"
+    << "- Execution Schedule : "<< s5.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
 
-  //Adding statement5
-  EvalSplineFComputation.addStmt(statement5);
+  //Adding s5
+  EvalSplineFComputation.addStmt(s5);
 
-  //Creating statement6
+  //Creating s6
   //double result = (1.0 - X) * Spline.f_vals[k] + X * Spline.f_vals[k+1] + X * (1.0 - X) * (A * (1.0 - X ) + B * X);
-  Stmt statement6("double result = (1.0 - X) * Spline.f_vals[k] + X * Spline.f_vals[k+1] + X * (1.0 - X) * (A * (1.0 - X ) + B * X)",
+  Stmt s6("double result = (1.0 - X) * Spline.f_vals[k] + X * Spline.f_vals[k+1] + X * (1.0 - X) * (A * (1.0 - X ) + B * X)",
       "{[]}",  //Iteration schedule - Only happening one time (not iterating)
-      "{[]->[5]}", //Execution schedule - scheduling statement to be first (scheduling function)
+      "{[0]->[5, 0, 0]}", //Execution schedule - scheduling statement to be first (scheduling function)
       {{"Spline.f_vals","{[k]->[k]}"}, {"Spline.f_vals","{[k+1]->[k+1]}"}}, //Data reads
       {}  //Data writes
       );
-  cout << "Source statement : " << statement6.getStmtSourceCode() << "\n\t"
-    <<"- Iteration Space : "<< statement6.getIterationSpace()->prettyPrintString() << "\n\t"
-    << "- Execution Schedule : "<< statement6.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
+  cout << "Source statement : " << s6.getStmtSourceCode() << "\n\t"
+    <<"- Iteration Space : "<< s6.getIterationSpace()->prettyPrintString() << "\n\t"
+    << "- Execution Schedule : "<< s6.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
 
-  //Adding statement6
-  EvalSplineFComputation.addStmt(statement6);
+  //Adding s6
+  EvalSplineFComputation.addStmt(s6);
 
-  //Creating statement7
+  //Creating s7
   //return result; 
-  Stmt statement7("return result",
+  Stmt s7("return result",
       "{[]}",  //Iteration schedule - Only happening one time (not iterating)
-      "{[]->[6]}", //Execution schedule - scheduling statement to be first (scheduling function)
+      "{[0]->[6, 0, 0]}", //Execution schedule - scheduling statement to be first (scheduling function)
       {},
       {}
       );
-  cout << "Source statement : " << statement7.getStmtSourceCode() << "\n\t"
-    <<"- Iteration Space : "<< statement7.getIterationSpace()->prettyPrintString() << "\n\t"
-    << "- Execution Schedule : "<< statement7.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
+  cout << "Source statement : " << s7.getStmtSourceCode() << "\n\t"
+    <<"- Iteration Space : "<< s7.getIterationSpace()->prettyPrintString() << "\n\t"
+    << "- Execution Schedule : "<< s7.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
 
-  //Adding statement7
-  EvalSplineFComputation.addStmt(statement7);
-
+  //Adding s7
+  EvalSplineFComputation.addStmt(s7);
+*/
   return EvalSplineFComputation;
+}
+
+
+/*
+  int Find_Segment(double x, double* x_vals, int length, int & prev){
+
+      for(int i = 0; i < length; i++){
+          if(x >= x_vals[i] && x <= x_vals[i+1]){
+              prev = i;
+          }
+      }
+      return prev;        
+  }
+*/
+Computation* Find_Segment_Computation(){
+
+  Computation* FindSegmentComputation;
+
+  //Creating s0
+  //if(x >= x_vals[i] && x <= x_vals[i+1]) prev = i;
+  Stmt s0("if(x >= x_vals[i] && x <= x_vals[i+1]) prev = i",
+      "{[i]: i>=0 && i<length}",  //Iteration schedule - Only happening one time (not iterating)
+      "{[i]->[0, i, 0]}", //Execution schedule - scheduling statement to be first (scheduling function)
+      {}, 
+      {}  
+      );  
+  cout << "Source statement : " << s0.getStmtSourceCode() << "\n\t"
+    <<"- Iteration Space : "<< s0.getIterationSpace()->prettyPrintString() << "\n\t"
+    << "- Execution Schedule : "<< s0.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
+
+  //Adding s0
+  FindSegmentComputation->addStmt(s0);
+
+  //Creating s1
+  //return prev; 
+  Stmt s1("return prev",
+      "{[]}",  //Iteration schedule - Only happening one time (not iterating)
+      "{[0]->[1, 0, 0]}", //Execution schedule - scheduling statement to be first (scheduling function)
+      {}, 
+      {}  
+      );  
+  cout << "Source statement : " << s1.getStmtSourceCode() << "\n\t"
+    <<"- Iteration Space : "<< s1.getIterationSpace()->prettyPrintString() << "\n\t"
+    << "- Execution Schedule : "<< s1.getExecutionSchedule()->prettyPrintString() << "\n\t" ;
+
+  //Adding s1
+  FindSegmentComputation->addStmt(s1);
+
+
+  return FindSegmentComputation;
 }
 
