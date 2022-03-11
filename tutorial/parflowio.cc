55d2edad6d8c6b4951938556e0ec01165a71d40d
kalyanbhetwal <kalyanbtl@gmail.com>
2022-03-11 12:34:05 -0700

    updated parflowio

--- tutorial/parflowio.cc
+++ tutorial/parflowio.cc
@@ -9,14 +9,6 @@ using namespace std;
 int main(int argc, char **argv){
 
 
-  /*void mttkrp(int I,int J, int K, int R,double *X,
-   *               double *A, double *B, double *C) {
-   * for (i = 0; i < I; i++)
-   *   for (j = 0; j < J; j++)
-   *     for (k = 0; k < K; k++)
-   *       for (r = 0; r < R; r++)
-   *         A[i,r] += X[i,j,k]*B[j,r]*C[k,r];
-  }*/
   vector < pair<string, string> > dataReads;
   vector < pair<string, string> > dataWrites;
   Computation parflowio;
