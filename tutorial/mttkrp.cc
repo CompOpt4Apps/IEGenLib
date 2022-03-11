76eb5eabe4a23e0c69927cdb16c572d691d408e4
Catherine Olschanowsky <catherineolschan@boisestate.edu>
2022-03-11 12:49:53 -0700

    this version of the code is core dumping

--- tutorial/mttkrp.cc
+++ tutorial/mttkrp.cc
@@ -9,30 +9,49 @@ using namespace std;
 int main(int argc, char **argv){
 
 
-  /*void mttkrp(int I,int J, int K, int R,double *X,
-   *               double *A, double *B, double *C) {
-   * for (i = 0; i < I; i++)
-   *   for (j = 0; j < J; j++)
-   *     for (k = 0; k < K; k++)
-   *       for (r = 0; r < R; r++)
-   *         A[i,r] += X[i,j,k]*B[j,r]*C[k,r];
-  }*/
+
+  // this one is dense
+  //void mttkrp(int I,int J, int K, int R,double *X,
+  //               double *A, double *B, double *C) {
+  // for (i = 0; i < I; i++)
+  //   for (j = 0; j < J; j++)
+  //     for (k = 0; k < K; k++)
+  //       for (r = 0; r < R; r++)
+  //         A[i,r] += X[i,j,k]*B[j,r]*C[k,r];
+  ///
   vector < pair<string, string> > dataReads;
   vector < pair<string, string> > dataWrites;
   Computation mttkrp;
+  mttkrp.addDataSpace("X","double*");
   mttkrp.addDataSpace("A","double*");
-  Stmt teststate("A(i) = 0",
-                  "{[i] : 0 <= i < I}",
-                  "{[i]->[0, i, 0]}",
+  mttkrp.addDataSpace("B","double*");
+  mttkrp.addDataSpace("C","double*");
+  Stmt s0("A(i,r) += X(i,j,k)*B(j,r)*C(k,r)",
+                  "{[i,j,k,r] : 0 <= i < I and 0<=j<J and 0<=k<K and 0<=r<R}",
+                  "{[i,j,k,r]->[0,i,0,j,0,k,0,r,0]}",
                    dataReads,
                    dataWrites);
 
-  mttkrp.addStmt(&teststate);
+  mttkrp.addStmt(&s0);
+
+  // this one is COO
+  Computation mttkrp_sps;
+  mttkrp_sps.addDataSpace("X","double*");
+  mttkrp_sps.addDataSpace("A","double*");
+  mttkrp_sps.addDataSpace("B","double*");
+  mttkrp_sps.addDataSpace("C","double*");
+  Stmt s1("A(x,i,j,k,r) += X(x,i,j,k,r)*B(x,i,j,k,r)*C(x,i,j,k,r)",
+                  "{[x,i,j,k,r] :  0<=x< NNZ and i=UFi(x) and j=UFj(x) and k=UFk(x) and 0<=r<R}",
+                  "{[x,i,j,k,r]->[0,x,0,i,0,j,0,k,0,r,0]}",
+                   dataReads,
+                   dataWrites);
+
+  mttkrp_sps.addStmt(&s1);
 
 
   //Calling 
   cout << "Codegen:\n";
-  cout << mttkrp.codeGen();
+  cout << mttkrp_sps.codeGen();
   
   return 0;
 }
