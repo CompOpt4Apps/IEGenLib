2ddefe2d5ef092eeaefaf4251c3a13bea6161574
kalyanbhetwal <kalyanbtl@gmail.com>
2022-03-15 12:21:46 -0600

    parflowio first statemet

--- tutorial/parflowio.cc
+++ tutorial/parflowio.cc
@@ -12,14 +12,37 @@ int main(int argc, char **argv){
   vector < pair<string, string> > dataReads;
   vector < pair<string, string> > dataWrites;
   Computation parflowio;
-  parflowio.addDataSpace("A","double*");
-  Stmt teststate("A(i) = 0",
-                  "{[i] : 0 <= i < I}",
-                  "{[i]->[0, i, 0]}",
-                   dataReads,
-                   dataWrites);
 
-  parflowio.addStmt(&teststate);
+  parflowio.addDataSpace("nsg","int");
+  parflowio.addDataSpace("x","int");
+  parflowio.addDataSpace("y","int");
+  parflowio.addDataSpace("z","int");
+  parflowio.addDataSpace("nx","int");
+  parflowio.addDataSpace("ny","int");
+  parflowio.addDataSpace("nz","int");
+  parflowio.addDataSpace("rx","int");
+  parflowio.addDataSpace("ry","int");
+  parflowio.addDataSpace("rz","int");
+  parflowio.addDataSpace("nsg","int");
+  parflowio.addDataSpace("errcheck","int");
+  parflowio.addDataSpace("x_overlap","int");
+  parflowio.addDataSpace("clip_x","int");
+  parflowio.addDataSpace("extent_x","int");
+
+
+  Stmt s1("x_overlap = fminl(clip_x+extent_x, x+nx) - fmaxl(clip_x,x)",
+          "{[0] : 0 <= nsg < m_numSubgrids}",
+          "{[i]->[0, i, 0]}",
+          {
+            {"clip_x", "{[0] -> [0]}"},
+            {"extent_x", "{[0] -> [0]}"},
+            {"x", "{[0] -> [0]}"},
+          },
+          {
+            {"x_overlap", "{[0] -> [0]}"}
+          });
+
+  parflowio.addStmt(&s1);
 
 
   //Calling 
